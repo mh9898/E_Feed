0d2b5c9dc6d96aa385b17f328b0625681913b82d
mh9898 <mh9898@gmail.com>
2021-06-06 19:54:02 +0300

    Initial Commit

---
+++ E_Feed/E_Feed.h
@@ -0,0 +1,18 @@
+//
+//  E_Feed.h
+//  E_Feed
+//
+//  Created by MiciH on 6/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for E_Feed.
+FOUNDATION_EXPORT double E_FeedVersionNumber;
+
+//! Project version string for E_Feed.
+FOUNDATION_EXPORT const unsigned char E_FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <E_Feed/PublicHeader.h>
+
+

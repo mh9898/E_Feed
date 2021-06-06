f6e71e9dc837f85afa2d55df32df80b955610caf
mh9898 <mh9898@gmail.com>
2021-06-06 22:48:45 +0300

    move that logic to a separate type 'HTTPClientSpay'

--- E_FeedTests/RemoteFeedLoaderTests.swift
+++ E_FeedTests/RemoteFeedLoaderTests.swift
@@ -9,51 +9,51 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestURL = URL(string: "https://a-url.com")
+        //removing the singleton
+        //2. move the test logic form RemoteFeedLoader -> HTTPClient
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient {
-    
-    static let shared = HTTPClient()
-    
-    private init() {}
-    
+    //removing the singleton
+    //1. make the shard instance a var -> global state
+    static var shared = HTTPClient()
+
+    func get(from url: URL){}
+}
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url: URL) {
+        requestURL = url
+    }
     var requestURL: URL?
 }
+
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
-        let client = HTTPClient.shared
-//        sut.load()
+
         
         XCTAssertNil(client.requestURL)
     }
     
     
     func test_load_requestDataFromURL(){
-        let client = HTTPClient.shared
+        
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         //system under test
         let sut = RemoteFeedLoader()
         
         sut.load()
         
         XCTAssertNotNil(client.requestURL)
-        
-        //option of dependency injection
-        //1.passing it in the constructor
-//        let sut1 = RemoteFeedLoader(client: client)
-        
-        
-        //2.option of dependency injection
-        //set in to property
-//        sut.client = client
-        
-        //2.option of dependency injection
-        //parma to the load function
-//        sut.client = client
+    
     }
     
 }

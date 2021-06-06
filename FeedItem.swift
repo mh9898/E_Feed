07330ab5c5cac2f505fb7a86f7f28d13a214bb31
mh9898 <mh9898@gmail.com>
2021-06-06 20:00:15 +0300

    create the feed modal item

---
+++ FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  E_Feed
+//
+//  Created by MiciH on 6/6/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

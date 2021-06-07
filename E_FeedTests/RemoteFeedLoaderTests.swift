fe9e7cb6f07c37170780406c79efc58993de0862
mh9898 <mh9898@gmail.com>
2021-06-07 12:22:18 +0300

    gave a url instead of hard code it in the RemoteFeedLoader

--- E_FeedTests/RemoteFeedLoaderTests.swift
+++ E_FeedTests/RemoteFeedLoaderTests.swift
@@ -10,14 +10,15 @@ import XCTest
 class RemoteFeedLoader {
     
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient){
+    init(url: URL, client: HTTPClient){
         self.client = client
+        self.url = url
     }
     
     func load() {
-
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -27,34 +28,42 @@ protocol HTTPClient {
 }
 
 class HTTPClientSpy: HTTPClient {
+    
+    var requestURL: URL?
+    
     func get(from url: URL) {
         requestURL = url
     }
-    var requestURL: URL?
+    
 }
 
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         
+        let url = URL(string: "https://a-url.com")!
+
         let client = HTTPClientSpy()
 //        HTTPClient.shared = client
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
 
         
         XCTAssertNil(client.requestURL)
     }
-    
-    
+        
     func test_load_requestDataFromURL(){
         
+        let url = URL(string: "https://a-given-url.com")!
+
+        
         let client = HTTPClientSpy()
-        //system under test
-        let sut = RemoteFeedLoader(client: client)
+        //sut = system under test
+        let remoteFeedLoader = RemoteFeedLoader(url: url, client: client)
         
-        sut.load()
+        remoteFeedLoader.load()
         
-        XCTAssertNotNil(client.requestURL)
+//        XCTAssertNotNil(client.requestURL)
+        XCTAssertEqual(client.requestURL, url)
     
     }
     
